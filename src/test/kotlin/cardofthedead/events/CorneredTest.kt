d6f66270958b20de0604a338c0ee6b9ef6112d18
kndykndy <eakrapivin@gmail.com>
2020-03-28 16:33:32 +0100

    Cornered tests

---
+++ src/test/kotlin/cardofthedead/events/CorneredTest.kt
@@ -0,0 +1,50 @@
+package cardofthedead.events
+
+import cardofthedead.TestUtils.addMovementPoints
+import cardofthedead.TestUtils.dummyPlayer
+import cardofthedead.TestUtils.gameWithDeck
+import cardofthedead.cards.EmptyDeck
+import cardofthedead.cards.actions.Armored
+import cardofthedead.cards.actions.Dynamite
+import cardofthedead.cards.events.Cornered
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class CorneredTest : StringSpec({
+
+    "should discard all escape cards" {
+        // given
+
+        val player = dummyPlayer().apply {
+            addMovementPoints(Armored(), Dynamite())
+        }
+
+        val game = gameWithDeck(player, EmptyDeck())
+
+        // when
+        player.play(Cornered().apply { gameContext = game })
+
+        // then
+
+        player.getMovementPointsCount() shouldBe 0
+
+        game.discardDeck.size() shouldBe 2 // Armored, Dynamite
+    }
+
+    "should not discard any cards if no escape cards" {
+        // given
+
+        val player = dummyPlayer()
+
+        val game = gameWithDeck(player, EmptyDeck())
+
+        // when
+        player.play(Cornered().apply { gameContext = game })
+
+        // then
+
+        player.getMovementPointsCount() shouldBe 0
+
+        game.discardDeck.size() shouldBe 0
+    }
+})
