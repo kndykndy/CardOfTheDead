6204b5b87471a3fd8d6b966afac5c15a1b71735c
kndykndy <eakrapivin@gmail.com>
2020-03-30 22:39:12 +0200

    Standard deck test

---
+++ src/test/kotlin/cardofthedead/cards/StandardDeckTest.kt
@@ -0,0 +1,28 @@
+package cardofthedead.cards
+
+import cardofthedead.cards.events.Cornered
+import cardofthedead.cards.zombies.`Zombies!!!`
+import cardofthedead.decks.StandardDeck
+import cardofthedead.decks.getActions
+import cardofthedead.game.Game
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+
+class StandardDeckTest : StringSpec({
+
+    "should " {
+        // given
+        val game = Game()
+
+        // when
+        val deck = StandardDeck(game)
+
+        // then
+
+        deck.cards.size shouldBe 56
+        deck.getActions() shouldHaveSize 27
+        deck.hasCardOfClass(Cornered::class.java) shouldBe true
+        deck.hasCardOfClass(`Zombies!!!`::class.java) shouldBe true
+    }
+})
