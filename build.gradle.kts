237f56604e7f0aa09c7ef7e00602bbd2b7f16dc3
kndykndy <eakrapivin@gmail.com>
2020-07-05 16:16:06 +0200

    Finalised build.gradle for distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+version = "0.1"
+
+// Check Java version, min 8 is required
+if (JavaVersion.current() < JavaVersion.VERSION_1_8)
+    throw RuntimeException("Min Java 8 required (running ${System.getProperty("java.version")})")
+
+// Log CotD, Gradle and Java versions
+println("Card Of The Dead $version")
+println("Gradle ${gradle.gradleVersion} at ${gradle.gradleHomeDir}")
+println("Java ${System.getProperty("java.version")}")
+
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    kotlin("jvm") version "1.3.60"
+    kotlin("jvm") version "1.3.70"
 
     // Apply the application plugin to add support for building a CLI application.
     application
@@ -39,4 +50,18 @@ tasks.withType<Test> {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Main-Class"] = application.mainClassName
+        attributes["Class-Path"] =
+            configurations.compileClasspath.get().joinToString(" ") { it.name }
+        attributes["Implementation-Version"] = archiveVersion
+    }
+
+    println("Collecting dependencies to a fat jar:")
+    from(configurations.compileClasspath.get().files
+        .map { println("  ${it.name}"); it }
+        .map { if (it.isDirectory) it else zipTree(it) })
 }
